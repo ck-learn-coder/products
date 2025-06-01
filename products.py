4dc948a9fde69208e9af34c0b6dd46254c2efa1d
ck <albertckchang@gmail.com>
2025-06-01 11:00:24 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,19 @@
+# 二維度清單
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+
+	np = [] #二維度清單
+	np.append(name)
+	np.append(price)
+
+	np=[name, price] #這樣寫也可以
+	products.append(np)
+	#products.append([name, price]) 這樣寫也可以
+
+print(products)
+
+products[0][0]
